0662bb73393e8f913ab5485293eaa021ee7722e2
milanm98 <marjanovmilan13@gmail.com>
2021-04-19 18:18:37 -0400

    pictures added

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,8 +1,14 @@
 function Content(){
     return(
-        <div className="text-5xl text-black bg-white font-black">
-            Najnovije : 
-        </div>
+        <main>
+            <div className="text-2xl text-center md:text-5xl text-black bg-white font-black">
+                Najnovije : 
+            </div>
+
+            <div className="text-center flex items-center justify-center w-5/6 h-auto">
+                Jelo
+            </div>
+        </main>
     )
 }
 
