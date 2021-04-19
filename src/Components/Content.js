48f8c314e787ad16eecd2a17b616f26105b1ffba
milanm98 <marjanovmilan13@gmail.com>
2021-04-19 19:25:42 -0400

    newest meals feature

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,12 +1,30 @@
+import torta from "../tortamaline.jpg";
+
 function Content(){
     return(
         <main>
-            <div className="text-2xl text-center md:text-5xl text-black bg-white font-black">
+            <div className="text-2xl my-5 text-center md:text-5xl text-black bg-white font-black">
                 Najnovije : 
             </div>
 
-            <div className="text-center flex items-center justify-center w-5/6 h-auto">
-                Jelo
+            <div className="flex flex-col items-center content-center justify-center w-full h-auto">
+                <div className="flex flex-col md:flex-row">
+                    <img src={torta}
+                     alt = "slika torte sa malinama"
+                     className="float-none md:float-left p-6"></img>
+                    
+                    <p className=" text-center text-1xl md:text-5xl">
+                        <strong><em>Rapsberry cake</em></strong><br></br><br></br>
+                        Sastojci :<br></br>
+                        1l jogurta<br></br>
+                        300g malina ili nekog drugog voca<br></br>
+                        700g petit keksa<br></br>
+                        250g kristal secera<br></br>
+                        300g slag krema<br></br>
+                        aroma maline
+                        </p>
+                    
+                </div>
             </div>
         </main>
     )
