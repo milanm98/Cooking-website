e18f2a47ba5f69043042d4c9a1f9d732999d9b04
milanm98 <marjanovmilan13@gmail.com>
2021-04-20 15:23:58 -0400

    icons

--- src/App.js
+++ src/App.js
@@ -11,13 +11,25 @@ function App() {
         <title> Moja Kuhinjica </title>
       </Helmet>
 
-      <div className="w-screen h-screen background"></div>
+      <div className="w-screen h-screen background">
+        <div className="flex flex-col float-left">
+          <a href="https://www.facebook.com/dragana.jerkov" rel="noreferrer" target="_blank" className="aria mx-10 my-5 fa fa-facebook-square text-white"><p className="hidden">a</p></a>
+          <a href="https://www.instagram.com/moja___kuhinjicaa/" rel="noreferrer" target="_blank" className="mx-10 my-5 fa fa-instagram text-white"><p className="hidden">a</p></a>
+          <a href="https://www.youtube.com/channel/UCO1Xf0omK274FCE6JEKflMQ/videos" rel="noreferrer" target="_blank" className="mx-10 my-5 fa fa-youtube text-white"><p className="hidden">a</p></a>
+          <a href="#najnovije" className="mx-10 my-5 fa fa-angellist text-white"><p className="hidden">a</p></a>
+          <a href="#najpopularnije" className="mx-10 my-5 fa fa-line-chart text-white"><p className="hidden">a</p></a>
+        </div>
+      </div>
       <div className="arrow animate-pulse text-white text-6xl md:text-9xl">
         <span>&dArr;</span>
       </div>
       <div className="content">
-        <NewestMeal></NewestMeal>
-        <MostPopularMeal></MostPopularMeal> 
+        <div>
+          <NewestMeal></NewestMeal>
+        </div>
+        <div>
+          <MostPopularMeal></MostPopularMeal> 
+        </div>
       </div>
     </main>
   );
