0ba80ab6c4df70c6dae937b8f00e13e55c5e98cd
milanm98 <marjanovmilan13@gmail.com>
2021-04-21 01:43:35 -0400

    galery feature done

--- src/Components/Galerija.js
+++ src/Components/Galerija.js
@@ -4,6 +4,26 @@ import galerija3 from "../galerija3.png";
 import galerija4 from "../galerija4.png";
 import galerija5 from "../galerija5.png";
 import galerija6 from "../galerija6.png";
+import galerija7 from "../galerija7.png";
+import galerija8 from "../galerija8.png";
+import galerija9 from "../galerija9.png";
+import galerija10 from "../galerija10.png";
+import galerija11 from "../galerija11.png";
+import galerija12 from "../galerija12.png";
+import galerija13 from "../galerija13.png";
+import galerija14 from "../galerija14.png";
+import galerija15 from "../galerija15.png";
+import galerija16 from "../galerija16.png";
+import galerija17 from "../galerija17.png";
+import galerija18 from "../galerija18.png";
+import galerija19 from "../galerija19.png";
+import galerija20 from "../galerija20.png";
+import galerija21 from "../galerija21.png";
+import galerija22 from "../galerija22.png";
+import galerija23 from "../galerija23.png";
+import galerija24 from "../galerija24.png";
+
+
 
 function Galerija(){
     return(
@@ -48,6 +68,114 @@ function Galerija(){
                 <div className="text-center w-5/6 md:w-1/2">
                     <p className="text-1xl md:text-3xl"><strong><em>Zebra kolac</em></strong></p>
                     <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija6}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Kakao kolac</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija7}></img>
+                </div>
+            </div>
+              <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Salata sa pecenom paprikom</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija8}></img>
+                </div>
+            </div>
+              <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Piletina sa belim lukom u sosu</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija9}></img>
+                </div>
+            </div>
+              <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Pita sa sirom</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija10}></img>
+                </div>
+            </div>
+              <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Pljeska burger</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija11}></img>
+                </div>
+            </div>
+              <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Snicle u sosu sa senfom</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija12}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Rol pica</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija13}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Cokoladni kolac</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija14}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Pasta karbonara</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija15}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Ledi di torta</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija16}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Karadjordjeva na moj nacin</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija17}></img>
+                </div>
+            </div>
+             <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Rizi bizi</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija18}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Rolnice</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija19}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Lovacke snicle</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija20}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Kokos gnezda</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija21}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Rol pica</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija22}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Rolat</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija23}></img>
+                </div>
+            </div>
+            <div className="p-3 md:p-12 flex flex-col items-center text-white font-black font-serif content-center justify-center w-full">
+                <div className="text-center w-5/6 md:w-1/2">
+                    <p className="text-1xl md:text-3xl"><strong><em>Mini krofnice</em></strong></p>
+                    <img className="w-full h-32 md:h-96" alt="oblande sa plazmom" src={galerija24}></img>
                 </div>
             </div>
         </main>
